ec2318f3cbde5cc4d0e1fb9b3239f11955c4b514
NingXinran <ningxinran@u.yale-nus.edu.sg>
2023-07-09 23:18:58 +0800

    Start project4

---
+++ project4/network/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    time = models.DateTimeField()
+    likes = models.IntegerField()
+    followers = models.ManyToManyField(User, related_name="followers", null=True)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
