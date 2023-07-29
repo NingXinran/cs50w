240ecd18275d2215f110b90afe850a209a053147
NingXinran <ningxinran@u.yale-nus.edu.sg>
2023-07-30 01:16:29 +0800

    Reset branch for fully django

--- ilovetehping/backend/ilovetehping/views.py
+++ ilovetehping/backend/ilovetehping/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import UserSerializer, ShopSerializer, RatingSerializer
 from .models import User, Shop, Rating
 
-class UserView(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-class ShopView(viewsets.ModelViewSet):
-    serializer_class = ShopSerializer
-    queryset = Shop.objects.all()
-
-class RatingView(viewsets.ModelViewSet):
-    serializer_class = RatingSerializer
-    queryset = Rating.objects.all()
+# add index view, map view here
